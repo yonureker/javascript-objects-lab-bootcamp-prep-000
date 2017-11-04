2fba79b52a3c229bb1bbfe7cc30bce908b9b1c69
Onur Eker <yonureker@gmail.com>
2017-11-04 06:17:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object[key] = value
+  object[key] = value;
+  return object;
 }
